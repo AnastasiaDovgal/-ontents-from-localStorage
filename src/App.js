c5dac02c82ce77a47de4c0e808399b7304fd7a6b
AnastasiaDovgal <koshka1621@gmail.com>
2019-12-23 20:20:33 +0200

    Initial logic and notes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MusicArtists from './MusicArtists';
 import Modal from './components/Modal';
+import Music from './components/Music';
 
 function App() {
-  const toLocalStorage = event => {
+  useEffect(() => {
+    const list = JSON.parse(JSON.stringify(MusicArtists));
+    localStorage.setItem(0, JSON.stringify(MusicArtists));
+
+    // localStorage.setItem(0, MusicArtists.artist_list);
+    console.log(localStorage.getItem(0));
+    console.log(list);
+
+    // useArtists(list);
+  }, [])
+
+  // [artists, useArtists] = useState(localStorage.getItem(0));
+  const [artists, useArtists] = useState({});
+
+
+  const handelForm = event => {
     event.preventDefault();
-    let l = event.target[0].value;
-    console.log(l);
+    let name = event.target[0].value;
+
+    // useArtists(list);
+
+    console.log(name);
   }
+
   return (
     <div>
-      <form onSubmit={toLocalStorage}>
-        <input type="text" placeholder="Search by name" />
+      <form onSubmit={handelForm}>
+        <input type="text" placeholder="Search by artist name" />
 
         <button type="submit">Press</button>
       </form>
+
 
 
     </div>
